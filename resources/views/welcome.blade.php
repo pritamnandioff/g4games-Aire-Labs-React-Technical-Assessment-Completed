fee049cd8741acd567e613bee6c74d16fc53cdc9
Pritam Nandi <pritamnandi@Pritams-MacBook-Pro.local>
2023-09-01 17:58:26 +0530

    technical assesment

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</head>
+
+<body style="background-color:#FFF1E8;">
+
+    <div style="width: 305px;
+    height: 800px;
+    top: 34%;
+    left: 0px;
+    position: fixed;
+    background-image: url(images/2.png);"></div>
+
+    <div style="    width: 322px;
+    height: 669px;
+    top: 0%;
+    right: -72px;
+    position: fixed;
+    background-image: url(images/1.png);
+    background-position: left;
+    background-size: contain;"></div>
+
+    <div class="container pt-5">
+        <div class="row">
+
+            <div class="col-sm-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="rating">
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill"></i>
+                        </div>
+                        <div class="comment">
+                            <p style="font-size:14px; margin-top:5px;">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                        </div>
+                        <div class="d-flex">
+                            <div class="circle">
+                                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" srcset="" style="max-width: 40px; ">
+                            </div>
+                            <div class="circle-text ml-4">
+                                <h5 class="mb-0" style="font-size: 16px;">Britten Wheeler</h5>
+                                <p style="font-size:13px;">Consultant</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="rating">
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill"></i>
+                        </div>
+                        <div class="comment">
+                            <p style="font-size:14px; margin-top:5px;">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                        </div>
+                        <div class="d-flex">
+                            <div class="circle">
+                                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" srcset="" style="max-width: 40px; ">
+                            </div>
+                            <div class="circle-text ml-4">
+                                <h5 class="mb-0" style="font-size: 16px;">Britten Wheeler</h5>
+                                <p style="font-size:13px;">Consultant</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="rating">
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill"></i>
+                        </div>
+                        <div class="comment">
+                        <p style="font-size:14px; margin-top:5px;">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                        </div>
+                        <div class="d-flex">
+                            <div class="circle">
+                                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" srcset="" style="max-width: 40px; ">
+                            </div>
+                            <div class="circle-text ml-4">
+                                <h5 class="mb-0" style="font-size: 16px;">Britten Wheeler</h5>
+                                <p style="font-size:13px;">Consultant</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="rating">
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill"></i>
+                        </div>
+                        <div class="comment">
+                        <p style="font-size:14px; margin-top:5px;">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                        </div>
+                        <div class="d-flex">
+                            <div class="circle">
+                                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" srcset="" style="max-width: 40px; ">
+                            </div>
+                            <div class="circle-text ml-4">
+                                <h5 class="mb-0" style="font-size: 16px;">Britten Wheeler</h5>
+                                <p style="font-size:13px;">Consultant</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="rating">
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill"></i>
+                        </div>
+                        <div class="comment">
+                        <p style="font-size:14px; margin-top:5px;">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                        </div>
+                        <div class="d-flex">
+                            <div class="circle">
+                                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" srcset="" style="max-width: 40px; ">
+                            </div>
+                            <div class="circle-text ml-4">
+                                <h5 class="mb-0" style="font-size: 16px;">Britten Wheeler</h5>
+                                <p style="font-size:13px;">Consultant</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="rating">
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill text-warning"></i>
+                            <i class="bi bi-star-fill"></i>
+                        </div>
+                        <div class="comment">
+                        <p style="font-size:14px; margin-top:5px;">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                        </div>
+                        <div class="d-flex">
+                            <div class="circle">
+                                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="" srcset="" style="max-width: 40px; ">
+                            </div>
+                            <div class="circle-text ml-4">
+                                <h5 class="mb-0" style="font-size: 16px;">Britten Wheeler</h5>
+                                <p style="font-size:13px;">Consultant</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+</body>
+
+</html>
